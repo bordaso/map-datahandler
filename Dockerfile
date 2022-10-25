17d6ce70dbbe360379fe01aa94304a9cf534680e
Oliver <zedasgm@gmail.com>
2022-10-25 22:34:39 +0200

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM openjdk:8
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} datahandler-0.0.1-SNAPSHOT.jar
-EXPOSE 8080
-EXPOSE 50051
+EXPOSE 8980
 ENTRYPOINT ["java","-jar","/datahandler-0.0.1-SNAPSHOT.jar"]
--- src/main/java/ec33nw/map/datahandler/DatahandlerApplication.java
+++ src/main/java/ec33nw/map/datahandler/DatahandlerApplication.java
@@ -11,13 +11,15 @@ import ec33nw.map.datahandler.controllers.iexcloud.IexCloudGrpcServer;
 
 @SpringBootApplication
 public class DatahandlerApplication {
-
+//kubectl exec --stdin --tty ec33nw-datahandler-769c8578df-tx9s8 -- bash
+// kubectl logs -f ec33nw-datahandler-769c8578df-tx9s8
 	public static void main(String[] args) {
 		SpringApplication.run(DatahandlerApplication.class, args);
 		
 		IexCloudGrpcServer server;
 		try {
-			server = new IexCloudGrpcServer(50051);
+			server = new IexCloudGrpcServer(8980);
+			System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>teszt        3");
 		    server.start();
 		    server.blockUntilShutdown();
 		} catch (IOException e) {
